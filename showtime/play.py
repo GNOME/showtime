fdd13b0d588d6bb92d337eedce9c82c3bb96c65c
kramo <git@kramo.page>
2025-05-17 14:43:39 +0200

    general: Replace play module

--- showtime/play.py
+++ showtime/play.py
@@ -4,11 +4,50 @@
 from typing import Any
 
 from gi.repository import (
+    Gdk,
     GObject,
     Gst,
     GstPbutils,
-    GstPlay,  # type: ignore
+    GstPlay,  # type: ignore  # type: ignore
+    Gtk,
 )
+
+from showtime import system, utils
+
+
+def gst_play_setup(
+    picture: Gtk.Picture,
+) -> tuple[Gdk.Paintable, GstPlay.Play, Gst.Element, Gst.Element]:
+    """Set up `GstPlay`."""
+    sink = paintable_sink = Gst.ElementFactory.make("gtk4paintablesink")
+    if not paintable_sink:
+        msg = "Cannot make gtk4paintablesink"
+        raise RuntimeError(msg)
+
+    paintable = paintable_sink.props.paintable  # type: ignore
+    picture.props.paintable = paintable
+
+    # OpenGL doesn't work on macOS properly
+    if paintable.props.gl_context and system != "Darwin":
+        gl_sink = Gst.ElementFactory.make("glsinkbin")
+        gl_sink.props.sink = paintable_sink  # type: ignore
+        sink = gl_sink
+
+    play = GstPlay.Play(
+        video_renderer=GstPlay.PlayVideoOverlayVideoRenderer.new_with_sink(None, sink)
+    )
+
+    pipeline = play.props.pipeline
+
+    def set_subtitle_font_desc(*_args: Any) -> None:
+        pipeline.props.subtitle_font_desc = utils.get_subtitle_font_desc()
+
+    if settings := Gtk.Settings.get_default():
+        settings.connect("notify::gtk-xft-dpi", set_subtitle_font_desc)
+
+    set_subtitle_font_desc()
+
+    return paintable, play, pipeline, paintable_sink
 
 
 class Messenger(GObject.Object):
@@ -54,14 +93,7 @@ class Messenger(GObject.Object):
             case GstPlay.PlayMessage.DURATION_CHANGED:
                 self.emit(
                     "duration-changed",
-                    (
-                        GstPlay.PlayMessage.parse_duration_changed
-                        if (
-                            (((version := Gst.version())[0] == 1) and (version[1] > 24))
-                            or version[0] > 1
-                        )
-                        else GstPlay.PlayMessage.parse_duration_updated
-                    )(msg),
+                    GstPlay.PlayMessage.parse_duration_changed(msg),
                 )
 
             case GstPlay.PlayMessage.POSITION_UPDATED:
